bce0ed7930a2bf95d6bd68fe46a58e6a598327c9
Bakhriev <legendofpain5112@gmail.com>
2022-10-04 22:39:38 +0500

    add Header, BurgerMenu

---
+++ src/components/Header/Hamburger/index.jsx
@@ -0,0 +1,23 @@
+import styles from "./Hamburger.module.scss";
+import { useState } from "react";
+
+function Hamburger() {
+  const [isOpened, setIsOpened] = useState(false);
+
+  function hamburgerCliked() {
+    setIsOpened(!isOpened);
+  }
+
+  return (
+    <div
+      onClick={hamburgerCliked}
+      className={`${styles.hamburger_layout} ${isOpened && styles.hamburger_hover_animate}`}
+    >
+      <span className={`${styles.bar_layout} ${styles.barTop}`}></span>
+      <span className={`${styles.bar_layout} ${styles.barMiddle}`}></span>
+      <span className={`${styles.bar_layout} ${styles.barBottom}`}></span>
+    </div>
+  );
+}
+
+export default Hamburger;
